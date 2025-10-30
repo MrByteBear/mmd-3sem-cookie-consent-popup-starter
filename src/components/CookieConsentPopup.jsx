55f7bed5f2bd816e533009414c076e1a71448c51
MrByteBear <bjornbytes95@gmail.com>
2025-10-30 11:11:10 +0100

    Functional cookie btn

--- src/components/CookieConsentPopup.jsx
+++ src/components/CookieConsentPopup.jsx
@@ -1,24 +1,31 @@
 "use client";
+import { useState } from "react";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { IoIosClose } from "react-icons/io";
+import CookieConsentButton from "./CookieConsentButton";
 
 const CookieConsentPopup = () => {
+  const [isAccepted, setIsAccepted] = useState(false);
   return (
+    !isAccepted && (
     <section className="cookie-consent-popup">
       <header className="cookie-consent-popup-header">
         <LiaCookieBiteSolid size={50} />
-        <IoIosClose className="pointer" size={25} />
+        <IoIosClose 
+        onClick={() => setIsAccepted(!isAccepted)}
+        className="pointer" 
+        size={25} />
       </header>
       <footer className="cookie-consent-popup-footer">
         <p className="cookie-consent-popup-paragraph">
           We use cookies to improve your user experience!
         </p>
-        <button className="cookie-consent-popup-cta pointer">
-          I like Cookies
-        </button>
+        <CookieConsentButton 
+        isAccepted={isAccepted} 
+        setIsAccepted={setIsAccepted}/>
       </footer>
     </section>
-  );
+  ));
 };
 
 export default CookieConsentPopup;
