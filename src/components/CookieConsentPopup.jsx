fed50bf2421c7679922bae1ee6951086602ff337
MrByteBear <bjornbytes95@gmail.com>
2025-10-30 11:16:08 +0100

    fixed

--- src/components/CookieConsentPopup.jsx
+++ src/components/CookieConsentPopup.jsx
@@ -2,27 +2,25 @@
 import { useState } from "react";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { IoIosClose } from "react-icons/io";
-import CookieConsentButton from "./CookieConsentButton";
 
 const CookieConsentPopup = () => {
   const [isAccepted, setIsAccepted] = useState(false);
   return (
-    !isAccepted && (
+    !isAccepted && !isClosed && (
     <section className="cookie-consent-popup">
       <header className="cookie-consent-popup-header">
         <LiaCookieBiteSolid size={50} />
-        <IoIosClose 
-        onClick={() => setIsAccepted(!isAccepted)}
-        className="pointer" 
-        size={25} />
+        <IoIosClose onClick={() => setIsAccepted(!isAccepted)} className="pointer" size={25} />
       </header>
       <footer className="cookie-consent-popup-footer">
         <p className="cookie-consent-popup-paragraph">
           We use cookies to improve your user experience!
         </p>
-        <CookieConsentButton 
-        isAccepted={isAccepted} 
-        setIsAccepted={setIsAccepted}/>
+        <button 
+        onClick={() => setIsAccepted(!isAccepted)}
+        className="cookie-consent-popup-cta pointer">
+          I like Cookies
+        </button>
       </footer>
     </section>
   ));
